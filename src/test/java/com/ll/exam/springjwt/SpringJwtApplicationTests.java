4c6b524c2ceadaceeef7cee2d030478afc899709
jihun-24k <gi020477@gmail.com>
2022-10-04 23:24:29 +0900

    Chore: 세팅

---
+++ src/test/java/com/ll/exam/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
